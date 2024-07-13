23f45d0f954784e5a6410742efc4f65f0579c652
crptcpchk <94843482+crptcpchk@users.noreply.github.com>
2024-07-13 13:33:35 +0300

    Update juneo-validator.sh

--- juneo-validator.sh
+++ juneo-validator.sh
@@ -6,26 +6,23 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
-sleep 1 && curl -s https://github.com/crptcpchk/utility-folder/blob/main/hlogo.sh | bash && sleep 1
+sleep 1 && curl -s https://raw.githubusercontent.com/crptcpchk/utility-folder/main/hlogo.sh | bash && sleep 1
 
 echo -e '\n\e[42mInstallin Docker & Docker Compose\e[0m\n' && sleep 1
 sudo apt install git docker.io docker-compose -y
-fi
+
 echo -e '\n\e[42mCloning GitHub repository\e[0m\n' && sleep 1
 cd && git clone https://github.com/Juneo-io/juneogo-binaries && sleep 1
-fi
 
 echo -e '\n\e[42mGranting execution permissions of the binary files\e[0m\n' && sleep 1
 chmod +x ~/juneogo-binaries/juneogo
 chmod +x ~/juneogo-binaries/plugins/jevm
 chmod +x ~/juneogo-binaries/plugins/srEr2XGGtowDVNQ6YgXcdUb16FGknssLTGUFYg7iMqESJ4h8e
-fi
 
 echo -e '\n\e[42mMoving jevm and srEr2XGGtowDVNQ6YgXcdUb16FGknssLTGUFYg7iMqESJ4h8e to .juneogo/plugins folder\e[0m\n' && sleep 1
 mkdir -p ~/.juneogo/plugins && \
 mv ~/juneogo-binaries/plugins/jevm ~/.juneogo/plugins && \
 mv ~/juneogo-binaries/plugins/srEr2XGGtowDVNQ6YgXcdUb16FGknssLTGUFYg7iMqESJ4h8e ~/.juneogo/plugins
-fi
 
 echo -e '\n\e[42mCreating a service file\e[0m\n' && sleep 1
 
@@ -43,7 +40,6 @@ LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target" > $HOME/juneod.service
 sudo mv $HOME/juneod.service /etc/systemd/system
-fi
 
 echo -e '\n\e[42mRunning a service\e[0m\n' && sleep 1
 sudo systemctl daemon-reload
