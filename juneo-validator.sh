621691fa3f055d73935fc18b76fcc89f2aa69436
crptcpchk <94843482+crptcpchk@users.noreply.github.com>
2024-07-13 13:36:29 +0300

    Update juneo-validator.sh

--- juneo-validator.sh
+++ juneo-validator.sh
@@ -6,7 +6,31 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
-sleep 1 && curl -s https://raw.githubusercontent.com/crptcpchk/utility-folder/main/hlogo.sh | bash && sleep 1
+sleep 1 
+echo -e '\e[40m\e[92m'
+echo -e '                                                            '
+echo -e ' /ψψ   /ψψ                     /ψψ                          '
+echo -e '| ψψ  | ψψ                    | ψψ                          '
+echo -e '| ψψ  | ψψ  /ψψψψψ/ψ  /ψψψψψψψ| ψψ   /ψψ  /ψψψψψψ  /ψ/ψψψψψ '
+echo -e '| ψψψψψψψψ /ψψ__  ψψ /ψψ_____/| ψψ /ψψ   /ψψ__  ψψ| ψψ__  ψψ'
+echo -e '| ψψ__  ψψ| ψψ  \ ψψ| ψψ      | ψψψψ    | ψψψψψψψψ| ψψ  \ ψψ'
+echo -e '| ψψ  | ψψ| ψψ  | ψψ| ψψ      | ψψ\ ψψ  | ψψ_____/| ψψ  | ψψ'
+echo -e '| ψψ  | ψψ|  ψψψψψ ψ| ψψψψψψψψ| ψψ \  ψψ|  ψψψψψψψ| ψψ  | ψψ'
+echo -e '|__/  |__/ \______/  \_______/|__/  \__/ \_______/|__/  |__/'
+echo -e '                                                            '
+echo -e '                /ψψψψψψ                                     '
+echo -e '               | ψψ___/ψψ                                   '
+echo -e '               | ψψ   |ψψ  /ψψψψψ/ψ  /ψψψψψψ                '
+echo -e '               | ψψ   |ψψ /ψψ__  ψψ /ψψ____ψψ               '
+echo -e '               | ψψ   |ψψ| ψψ  \ ψψ| ψψ   |ψψ               '
+echo -e '               | ψψ   |ψψ| ψψ  | ψψ| ψψ   |ψψ               '
+echo -e '               | ψψψψψψ /|  ψψψψψ ψ|  ψψψψψψ/               '
+echo -e '                \_____/   \______/ \_______/                '
+echo -e '                                                            '
+echo -e '                       and @crptcpchk                       '
+echo -e '\e[0m'
+
+sleep 4
 
 echo -e '\n\e[42mInstallin Docker & Docker Compose\e[0m\n' && sleep 1
 sudo apt install git docker.io docker-compose -y
